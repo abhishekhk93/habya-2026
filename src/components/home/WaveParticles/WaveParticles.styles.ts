8044835a8fb5480db052c5dd11d40d8a397ef1c9
Abhishek H K <abhishek.h.k@walmart.com>
2026-03-11 01:04:27 +0530

    fix wave component on home page

--- src/components/home/WaveParticles/WaveParticles.styles.ts
+++ src/components/home/WaveParticles/WaveParticles.styles.ts
@@ -9,6 +9,6 @@ export const waveParticlesStyles = {
         "ease-in-out",
     ].join(" "),
 
-    visible: "opacity-30", // More transparent
+    visible: "opacity-[0.20]", // Between 10% and 20%
     hidden: "opacity-0",
 } as const;
